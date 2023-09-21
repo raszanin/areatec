5ba2d09feac8714242c2d95cb9625fff0027bd30
Roberto Zanin <raszanin@gmail.com>
2023-09-21 13:07:04 -0300

    layout: forgot

--- src/app/forgot/page.tsx
+++ src/app/forgot/page.tsx
@@ -1,49 +1,44 @@
 'use client'
 
 import Link from 'next/link';
-import { ArrowCircleLeft, SignIn } from '@phosphor-icons/react';
+import { ArrowCircleLeft, Key } from '@phosphor-icons/react';
 import { Input } from "@/components/ui/input"
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 
-export default function Login() {
+export default function Forgot() {
   function login(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    alert('Em construção. Obrigado por nos visitar 😊')
-    console.log('login')
+    alert('Em breve, sua senha será enviada para seu e-mail. Obrigado por nos visitar 😊')
+    console.log('forgot')
   }
 
 
   return (
     <div className="flex flex-col h-full bg-camera bg-cover">
       <div className='flex justify-start gap-2 p-2 bg-black/50'>
-        <Link href='/' className='flex gap-2'>
+        <Link href='/login' className='flex gap-2'>
           <ArrowCircleLeft size={24} />
-          <span>Home</span>
+          <span>Login</span>
         </Link>
       </div>
       <main className="flex flex-1 justify-center items-center p-6">
         <form onSubmit={login} className="flex flex-col w-full md:w-96 gap-4 bg-black/50 p-6 rounded-lg">
-          <div className='flex justify-center'><h1>Área do Cliente</h1></div>
+          <div className='flex justify-center'>
+            <h1>Recuperar Senha</h1>
+          </div>
 
 
           <div>
             <Label htmlFor='email'>Usuário</Label>
             <Input type='email' ></Input>
           </div>
-          <div>
-            <Label htmlFor='email'>Senha</Label>
-            <Input type='password' />
-          </div>
 
           <Button className='gap-2'>
-            Entrar
-            <SignIn size={24} />
+            Enviar
+            <Key size={24} />
           </Button>
 
-          <div className='flex text-sm justify-center items-centers text-slate-300'>
-            <Link href={'/forgot'}>Esqueci a senha</Link>
-          </div>
 
 
 
